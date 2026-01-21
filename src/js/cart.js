ea60a7f28e2ae515e5ee83d6b4b42cacb7f4cfc3
Igor2games <igor1games@outlook.com>
2026-01-21 20:16:46 -0300

    Week 2 Group Activity

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,12 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage, loadHeaderFooter } from "./utils.mjs";
 
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart") || [];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
 }
+
+loadHeaderFooter();
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
 const element = document.querySelector(".product-list");
 
+loadHeaderFooter();
 const productList = new ProductList("Tents", dataSource, element);
 
 productList.init();
