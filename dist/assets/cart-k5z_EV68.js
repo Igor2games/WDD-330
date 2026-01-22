792f921b722f268690119b0053e57898a446774f
Igor2games <igor1games@outlook.com>
2026-01-21 22:14:06 -0300

    build

---
+++ dist/assets/cart-k5z_EV68.js
@@ -0,0 +1,15 @@
+import{l as s,g as c}from"./utils-B7drxXma.js";s();function e(){const t=(c("so-cart")||[]).map(r=>o(r));document.querySelector(".product-list").innerHTML=t.join("")}function o(a){return`<li class="cart-card divider" data-id="${a.Id}">
+  <a href="#" class="cart-card__image">
+    <img
+      src="${a.Images.PrimaryMedium}"
+      alt="${a.Name}"
+    />
+  </a>
+  <a href="#">
+    <h2 class="card__name">${a.Name}</h2>
+  </a>
+  <p class="cart-card__color">${a.Colors[0].ColorName}</p>
+  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__price">$${a.FinalPrice}</p>
+  <button class="cart-card__remove" aria-label="Remove ${a.Name} from cart">×</button>
+</li>`}function n(a){let t=c("so-cart")||[];t=t.filter(r=>r.Id!==a),localStorage.setItem("so-cart",JSON.stringify(t)),e()}document.addEventListener("click",a=>{if(a.target.classList.contains("cart-card__remove")){const t=a.target.closest(".cart-card").dataset.id;n(t)}});e();
